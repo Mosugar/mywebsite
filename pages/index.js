d5e851fcea6118d08de4d785e5a8f766af81f542
Mohamed sekkar <mohamedsekkar1110@gmail.com>
2025-07-05 20:27:38 +0100

    Fix component imports with .jsx extensions

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,11 @@
-import Hero from '../components/Hero';
-import Skills from '../components/Skills';
-import MatrixRain from '../components/MatrixRain';
-import Footer from '../components/Footer';
-import Work from '../components/Work';
-import Services from '../components/Services';
-import GlobalBackground from '../components/Layout';
-import ExperienceHistory from '../components/ExperienceHistory';
+import Hero from '../components/Hero.jsx';
+import Skills from '../components/Skills.jsx';
+import MatrixRain from '../components/MatrixRain.jsx';
+import Footer from '../components/Footer.jsx';
+import Work from '../components/Work.jsx';
+import Services from '../components/Services.jsx';
+import GlobalBackground from '../components/Layout.jsx';
+import ExperienceHistory from '../components/ExperienceHistory.jsx';
 
 export default function Home() {
   return (
